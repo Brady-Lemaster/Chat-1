d9a1a7e03f7dee2c4cab7dc92a6ff9888e7b4213
Frogzunian <99221369+Huzunian@users.noreply.github.com>
2023-11-09 09:45:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const CLIENT_ID = 'hWmT5ZuGe51IgiMD';
 
 const drone = new ScaleDrone(CLIENT_ID, {
   data: { // Will be sent out as clientData via events
-    name: 'Femboy'
+    name: 'Femboy',
+    color; '#000'
   },
 });
 
